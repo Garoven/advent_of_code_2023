c9b99331944b51e94b7d3e75c6f00edd524c8077
Garoven <jaceksobczak803@gmail.com>
2023-12-03 20:12:30 +0100

    small change to macro

--- src/main.rs
+++ src/main.rs
@@ -12,24 +12,24 @@ struct Args {
     day: u8,
 }
 
-macro_rules! print_day {
-    ($day:tt, $i:expr) => {
-        println!(
-            "Part one: {:?}\nPart two: {:?}",
-            $day::part_1($i),
-            $day::part_2($i)
-        )
-    };
-}
-
 fn main() -> advent_of_code_2023::Result<()> {
     let args = Args::parse();
     let input = &std::fs::read_to_string(args.input)?;
 
+    macro_rules! print_day {
+        ($day:tt) => {
+            println!(
+                "Part one: {:?}\nPart two: {:?}",
+                $day::part_1(input),
+                $day::part_2(input)
+            )
+        };
+    }
+
     match args.day {
-        1 => print_day!(day_01, input),
-        2 => print_day!(day_02, input),
-        3 => print_day!(day_03, input),
+        1 => print_day!(day_01),
+        2 => print_day!(day_02),
+        3 => print_day!(day_03),
         4..=25 => unimplemented!(),
         _ => unreachable!(),
     };
