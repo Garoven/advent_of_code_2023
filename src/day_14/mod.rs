4374f2b24b4884ab61e119a121ae0414c2d7b4fe
Garoven <jaceksobczak803@gmail.com>
2023-12-17 00:30:36 +0100

    day_17

--- src/day_14/mod.rs
+++ src/day_14/mod.rs
@@ -1,5 +1,93 @@
 use itertools::Itertools;
 
+/// Finds first appearance of pattern and it's length
+/// -> (start_of_pattern, length_of_pattern)
+fn brent<T, F>(f: F, x0: T) -> (u64, u64)
+where
+    T: Clone + Eq + PartialEq,
+    F: Fn(T) -> T,
+{
+    let mut power = 1;
+    let mut lam = 1;
+    let mut tortoise = x0.clone();
+    let mut hare = f(x0.clone());
+
+    while tortoise != hare {
+        if power == lam {
+            tortoise = hare.clone();
+            power *= 2;
+            lam = 0;
+        }
+
+        hare = f(hare);
+        lam += 1;
+    }
+
+    tortoise = x0.clone();
+    hare = x0;
+
+    for _ in 0..lam {
+        hare = f(hare);
+    }
+
+    let mut mu = 1;
+    while tortoise != hare {
+        tortoise = f(tortoise);
+        hare = f(hare);
+        mu += 1;
+    }
+
+    (mu, lam)
+}
+
+fn cycle(mut rows: Vec<String>) -> Vec<String> {
+    let mut cols = (0..rows[0].len())
+        .map(|col| rows.iter().map(|s| &s[col..=col]).join(""))
+        .collect_vec();
+
+    cols.iter_mut().enumerate().for_each(|(i, col)| {
+        *col = rows
+            .iter()
+            .map(|s| &s[i..=i])
+            .join("")
+            .split('#')
+            .map(|s| s.chars().sorted().join(""))
+            .join("#");
+    });
+
+    rows.iter_mut().enumerate().for_each(|(i, row)| {
+        *row = cols
+            .iter()
+            .map(|col| &col[i..=i])
+            .join("")
+            .split('#')
+            .map(|s| s.chars().sorted_by(|a, b| b.cmp(a)).join(""))
+            .join("#")
+    });
+
+    cols.iter_mut().enumerate().for_each(|(i, col)| {
+        *col = rows
+            .iter()
+            .map(|row| &row[i..=i])
+            .join("")
+            .split('#')
+            .map(|s| s.chars().sorted_by(|a, b| b.cmp(a)).join(""))
+            .join("#")
+    });
+
+    rows.iter_mut().enumerate().for_each(|(i, row)| {
+        *row = cols
+            .iter()
+            .map(|col| &col[i..=i])
+            .join("")
+            .split('#')
+            .map(|s| s.chars().sorted().join(""))
+            .join("#")
+    });
+
+    rows
+}
+
 pub fn part_1<T: AsRef<str>>(input: T) -> u64 {
     let rows = input
         .as_ref()
@@ -32,14 +120,12 @@ pub fn part_2<T: AsRef<str>>(input: T) -> u64 {
         .map(|l| l.to_string())
         .collect_vec();
 
-    let mut cols = (0..rows[0].len())
-        .map(|col| rows.iter().map(|s| &s[col..=col]).join(""))
-        .collect_vec();
-
-    let mut i = 1000;
+    let (skip, cycle_len) = brent(cycle, rows.clone());
+    let left_cycles = (1_000_000_000 - skip) % cycle_len;
+    let mut cycles = skip + cycle_len + left_cycles;
     loop {
-        if i != 0 {
-            i -= 1;
+        if cycles != 0 {
+            cycles -= 1;
         } else {
             return rows
                 .iter()
@@ -48,45 +134,7 @@ pub fn part_2<T: AsRef<str>>(input: T) -> u64 {
                 .sum::<usize>() as u64;
         }
 
-        cols.iter_mut().enumerate().for_each(|(i, col)| {
-            *col = rows
-                .iter()
-                .map(|s| &s[i..=i])
-                .join("")
-                .split('#')
-                .map(|s| s.chars().sorted().join(""))
-                .join("#");
-        });
-
-        rows.iter_mut().enumerate().for_each(|(i, row)| {
-            *row = cols
-                .iter()
-                .map(|col| &col[i..=i])
-                .join("")
-                .split('#')
-                .map(|s| s.chars().sorted_by(|a, b| b.cmp(a)).join(""))
-                .join("#")
-        });
-
-        cols.iter_mut().enumerate().for_each(|(i, col)| {
-            *col = rows
-                .iter()
-                .map(|row| &row[i..=i])
-                .join("")
-                .split('#')
-                .map(|s| s.chars().sorted_by(|a, b| b.cmp(a)).join(""))
-                .join("#")
-        });
-
-        rows.iter_mut().enumerate().for_each(|(i, row)| {
-            *row = cols
-                .iter()
-                .map(|col| &col[i..=i])
-                .join("")
-                .split('#')
-                .map(|s| s.chars().sorted().join(""))
-                .join("#")
-        });
+        rows = cycle(rows);
     }
 }
 
